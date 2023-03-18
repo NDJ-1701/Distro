4e1d6a32bcc01199d0cb2e7c73b420e7a813a3d0
NoahJohnston <35201520+NDJ-1701@users.noreply.github.com>
2023-03-18 21:56:36 +0000

    added basic login page, not connected to anything.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
-
-appName = "Distro"
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "Hello,world"
+def page_home():
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug = True)
+    app.run(host='0.0.0.0', debug=True)
